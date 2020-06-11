0015bdd806c45885654e861f57bb9c8db03e2062
Alex Tan <alextan785@gmail.com>
2020-06-11 07:25:12 -0700

    add logging to deployment script

--- Server/deploy.sh
+++ Server/deploy.sh
@@ -1,15 +1,20 @@
 echo "compiling..."
 tsc
+
+echo "installing producting modules..."
 rm -r node_modules
 npm install -production
 
+echo "zipping packages..."
 cd build
 zip -r ../scrape.zip .
 cd ..
 zip -ur scrape.zip node_modules
 
+echo "installing dev dependencies for local machine..."
 npm install --only=dev
 
+echo "uploading deployment package..."
 aws s3 rm s3://alexstlambda/scrape.zip
 aws s3 cp scrape.zip s3://alexstlambda
 
